0a090f90118de8f19bc29c4e66dceec3e1f89563
tulathorn <tul.tulathorn@gmail.com>
2018-11-22 15:27:06 +0700

    [ADD]: ADD user model

---
+++ API/users/model.js
@@ -0,0 +1,44 @@
+const sequelize = require("../sequelize");
+
+const UserModel = sequelize.define(
+  "tablename",
+  {
+    UserID: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      allowNull: false
+    },
+    FirstName: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    LastName: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    Role: {
+      type: Sequelize.INTEGER,
+      allowNull: false
+    },
+    Status: {
+      type: Sequelize.BOOLEAN,
+      allowNull: false
+    }
+  },
+  {
+    freezeTableName: true
+  }
+);
+
+module.exports = {
+  getUser = args => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        let data = await MeterModel.findAll({})
+        resolve(data)
+      } catch (err) {
+        reject(err)
+      }
+    })
+  }
+}
