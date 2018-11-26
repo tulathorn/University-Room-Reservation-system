a69f37b21fc357d6faa0b58aaac5ad45362d4340
tulathorn <tul.tulathorn@gmail.com>
2018-11-26 22:35:03 +0700

    [ADD]: Add swagger

--- API/routers.js
+++ API/routers.js
@@ -1,5 +1,20 @@
+const swaggerJSDoc = require("swagger-jsdoc");
+
 const Mainrouters = (fastify, opts, next) => {
-  // Test route
+  /**
+   * @swagger
+   * /:
+   *   get:
+   *     tags:
+   *       - root
+   *     description: Root of API
+   *     produces:
+   *       - application/json
+   *     responses:
+   *       200:
+   *         description: An array of users
+   *
+   */
   fastify.get("/", async (request, reply) => {
     reply.type("application/json").code(200);
     reply.send({ messege: "Welcome to RRS APi" });
--- API/server.js
+++ API/server.js
@@ -1,4 +1,33 @@
 const fastify = require("fastify")();
+const swaggerJSDoc = require("swagger-jsdoc");
+
+// Doc
+const swaggerDefinition = {
+  info: {
+    title: "Node Swagger API",
+    version: "1.0.0",
+    description: "Demonstrating how to describe a RESTful API with Swagger"
+  },
+  host: "localhost:4000",
+  basePath: "/"
+};
+
+// options for the swagger docs
+const options = {
+  // import swaggerDefinitions
+  swaggerDefinition: swaggerDefinition,
+  // path to the API docs
+  apis: ["./routes/.js", "routes.js"] // pass all in array
+};
+// initialize swagger-jsdoc
+const swaggerSpec = swaggerJSDoc(options);
+
+// Init swagger route
+fastify.get("/swagger.json", async (request, reply) => {
+  reply.code(200);
+  reply.header("Content-Type", "application/json");
+  reply.send(swaggerSpec);
+});
 
 // Health check the api
 fastify.get("/healthcheck", async (request, reply) => {
