32e053213b4ebd20f986cec60c1fa40fab951452
tulathorn <tul.tulathorn@gmail.com>
2019-01-21 15:16:00 +0700

    [ADD]: Fix add freezeTableName: true,timestamps: false in all model

--- API/Contact/model.js
+++ API/Contact/model.js
@@ -1,22 +1,29 @@
 const Sequelize = require("sequelize");
 const sequelize = require("../sequelize");
 
-const ContactModel = sequelize.define("Contact", {
-  ContactID: {
-    type: Sequelize.INTEGER,
-    allowNull: false,
-    autoIncrement: true
+const ContactModel = sequelize.define(
+  "Contact",
+  {
+    ContactID: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      autoIncrement: true
+    },
+    EmailAddress: {
+      type: Sequelize.STRING
+    },
+    Title: {
+      type: Sequelize.STRING
+    },
+    Detail: {
+      type: Sequelize.STRING
+    },
+    DateTime: {
+      type: Sequelize.DATE
+    }
   },
-  EmailAddress: {
-    type: Sequelize.STRING
-  },
-  Title: {
-    type: Sequelize.STRING
-  },
-  Detail: {
-    type: Sequelize.STRING
-  },
-  DateTime: {
-    type: Sequelize.DATE
+  {
+    freezeTableName: true,
+    timestamps: false
   }
-});
+);
--- API/section/model.js
+++ API/section/model.js
@@ -1,23 +1,30 @@
 const Sequelize = require("sequelize");
 const sequelize = require("../sequelize");
 
-const SectuonModel = sequelize.define("Section", {
-  ID: {
-    type: Sequelize.INTEGER,
-    primaryKey: true,
-    allowNull: false,
-    autoIncrement: true
+const SectuonModel = sequelize.define(
+  "Section",
+  {
+    ID: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true
+    },
+    Section: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    Year: {
+      type: Sequelize.INTEGER,
+      allowNull: false
+    },
+    Program: {
+      type: Sequelize.INTEGER,
+      allowNull: false
+    }
   },
-  Section: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  Year: {
-    type: Sequelize.INTEGER,
-    allowNull: false
-  },
-  Program: {
-    type: Sequelize.INTEGER,
-    allowNull: false
+  {
+    freezeTableName: true,
+    timestamps: false
   }
-});
+);
