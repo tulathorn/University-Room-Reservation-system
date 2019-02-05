a4e1846f28306d7ba91379b371f9508049887cc9
tulathorn <tul.tulathorn@gmail.com>
2019-02-06 01:02:51 +0700

    [ADD]: Add model for recurring

--- API/recurring/model.js
+++ API/recurring/model.js
@@ -1,11 +1,8 @@
 const Sequelize = require('sequelize')
 const sequelize = require('../sequelize')
 
-const { UserModel } = require('../users/model')
-const { RoomInformationModel } = require('../rooms/model')
-
-const ReservationsModel = sequelize.define(
-  'Reservations',
+const RecurringReservationsModel = sequelize.define(
+  'RecurringReservations',
   {
     BookingID: {
       type: Sequelize.INTEGER,
@@ -25,11 +22,19 @@ const ReservationsModel = sequelize.define(
       type: Sequelize.STRING,
       allowNull: false
     },
+    Term: {
+      type: Sequelize.CHAR,
+      allowNull: false
+    },
     Day: {
       type: Sequelize.INTEGER,
       allowNull: false
     },
-    Date: {
+    StartDate: {
+      type: Sequelize.DATEONLY,
+      allowNull: false
+    },
+    EndDate: {
       type: Sequelize.DATEONLY,
       allowNull: false
     },
@@ -40,6 +45,12 @@ const ReservationsModel = sequelize.define(
     EndTime: {
       type: Sequelize.TIME,
       allowNull: false
+    },
+    Sections: {
+      type: Sequelize.INTEGER
+    },
+    SecChar: {
+      type: Sequelize.CHAR
     },
     DateBooked: {
       type: Sequelize.DATE,
@@ -55,20 +66,12 @@ const ReservationsModel = sequelize.define(
   }
 )
 
-ReservationsModel.belongsTo(UserModel, { foreignKey: 'UserID' })
-ReservationsModel.belongsTo(RoomInformationModel, { foreignKey: 'RoomID' })
-
 module.exports = {
-  getAllReservations: args => {
+  getRecurring: args => {
     return new Promise(async (resolve, reject) => {
       try {
-        // console.log(args)
-        let data = await ReservationsModel.findAll({
-          where: { ...args },
-          include: [
-            { model: UserModel, foreignKey: 'UserID' },
-            { model: RoomInformationModel, foreignKey: 'RoomID' }
-          ]
+        let data = await RecurringReservationsModel.findAll({
+          where: { ...args }
         })
         resolve(data)
       } catch (err) {
@@ -77,36 +80,39 @@ module.exports = {
       }
     })
   },
-  createReservation: args => {
+  createRecurring: args => {
     return new Promise(async (resolve, reject) => {
       try {
-        let data = await ReservationsModel.create(args)
+        let data = await RecurringReservationsModel.create(args)
         resolve(data)
       } catch (err) {
+        console.log(err)
         reject(err)
       }
     })
   },
-  updateReservation: args => {
+  updateRecurring: args => {
     return new Promise(async (resolve, reject) => {
       try {
-        let data = await ReservationsModel.update(args, {
+        let data = await RecurringReservationsModel.update(args, {
           where: { BookingID: args.BookingID }
         })
         resolve(data)
       } catch (err) {
+        console.log(err)
         reject(err)
       }
     })
   },
-  deleteReservation: args => {
+  deleteRecurring: args => {
     return new Promise(async (resolve, reject) => {
       try {
-        let data = await ReservationsModel.destroy({
+        let data = await RecurringReservationsModel.destroy(args, {
           where: { BookingID: args.BookingID }
         })
         resolve(data)
       } catch (err) {
+        console.log(err)
         reject(err)
       }
     })
