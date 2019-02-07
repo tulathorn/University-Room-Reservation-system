90da31d203e816d000fe2ba99d302a29eb7d5b64
tulathorn <tul.tulathorn@gmail.com>
2019-02-08 00:16:24 +0700

    [ADD]: ADD model control for contact

--- API/Contact/model.js
+++ API/Contact/model.js
@@ -1,8 +1,8 @@
-const Sequelize = require("sequelize");
-const sequelize = require("../sequelize");
+const Sequelize = require('sequelize')
+const sequelize = require('../sequelize')
 
 const ContactModel = sequelize.define(
-  "Contact",
+  'Contact',
   {
     ContactID: {
       type: Sequelize.INTEGER,
@@ -26,4 +26,53 @@ const ContactModel = sequelize.define(
     freezeTableName: true,
     timestamps: false
   }
-);
+)
+
+module.exports = {
+  getContacts: args => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        let data = await ContactModel.findAll({ where: { ...args } })
+        resolve(data)
+      } catch (err) {
+        console.log(err)
+        reject(err)
+      }
+    })
+  },
+  createContact: args => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        let data = await ContactModel.create(args)
+        resolve(data)
+      } catch (err) {
+        console.log(err)
+        reject(err)
+      }
+    })
+  },
+  updateContact: args => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        let data = await ContactModel.update(args, {
+          where: { ContactID: args.ContactID }
+        })
+        resolve(data)
+      } catch (err) {
+        console.log(err)
+        reject(err)
+      }
+    })
+  },
+  deleteContact: args => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        let data = await ContactModel.destroy({ where: { ContactID: args.ContactID } })
+        resolve(data)
+      } catch (err) {
+        console.log(err)
+        reject(err)
+      }
+    })
+  }
+}
