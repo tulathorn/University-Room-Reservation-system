2c04d9a5d57285af4fe2f9263150eecdd5893319
tulathorn <tul.tulathorn@gmail.com>
2019-03-12 16:15:13 +0700

    [EDIT]: EDIT reservation

--- API/reservations/model.js
+++ API/reservations/model.js
@@ -1,41 +1,38 @@
-const Sequelize = require('sequelize')
-const sequelize = require('../sequelize')
+const Bookshelf = require('../bookshelf')
 
-const ReservationsSchema = require('./schema')
-const { UserModel } = require('../users/model')
-const { RoomInformationModel } = require('../rooms/model')
+const Reservation = Bookshelf.model('Reservation', {
+  tableName: 'Reservations',
+  UserInfo: function() {
+    return this.belongsToMany(User, 'UserID', 'UserID')
+  },
+  RoomInformation: function() {
+    return this.belongsTo(Room, 'RoomID', 'RoomID')
+  }
+})
 
-ReservationsSchema.associate = model => {
-  model.ReservationsSchema.belongsTo(model.UserModel, { foreignKey: 'UserID' }),
-    model.ReservationsSchema.belongsTo(model.RoomInformationModel, {
-      onDelete: 'CASCADE',
-      foreignKey: {
-        allowNull: false
-      }
-    })
-}
+const User = Bookshelf.model('User', {
+  tableName: 'UserInfo'
+})
 
-// ReservationsModel.associate = model => {
-//   model.ReservationsModel.belongsTo(UserModel, { foreignKey: 'UserID' })
-// }
+const Equipment = Bookshelf.model('Equipment', {
+  tableName: 'Equipment'
+})
 
-// ReservationsModel.belongsTo(UserModel, { foreignKey: 'UserID' })
-// ReservationsModel.hasOne(RoomInformationModel, { foreignKey: 'RoomID' })
-// ReservationsModel.belongsTo(RoomInformationModel, { foreignKey: 'RoomID' })
+const Room = Bookshelf.model('Room', {
+  tableName: 'RoomInformation',
+  Equipment: function() {
+    return this.belongsTo(Equipment, 'RoomID', 'RoomID')
+  }
+})
 
 module.exports = {
   getAllReservations: args => {
     return new Promise(async (resolve, reject) => {
       try {
-        // console.log(args)
-        let data = await ReservationsSchema.findAll({
-          where: { ...args },
-          include: [
-            { model: UserModel, foreignKey: 'UserID' },
-            { model: RoomInformationModel, foreignKey: 'RoomID' }
-          ]
+        const Reservations = Reservation.where({ ...args }).fetchAll({
+          withRelated: ['User', 'Room']
         })
-        resolve(data)
+        resolve(Reservations)
       } catch (err) {
         console.log(err)
         reject(err)
