c2aa2f45816bdd99844d2f660cfbb3eb324a4bb7
tulathorn <tul.tulathorn@gmail.com>
2019-03-14 14:44:31 +0700

    [FIX]; Fix room and seperate to model repo controller

--- API/rooms/controller.js
+++ API/rooms/controller.js
@@ -1,14 +1,14 @@
-const RoomModel = require('./model')
+const RoomRepo = require('./repository')
 const R = require('ramda')
 
 module.exports = {
   find: async data => {
     console.log(data)
     if (data) {
-      let response = await RoomModel.getRoom(data)
+      let response = await RoomRepo.getRoom(data)
       return response
     }
-    let response = await RoomModel.getAllRooms()
+    let response = await RoomRepo.getAllRooms()
     return response
   },
   create: async data => {
@@ -39,7 +39,7 @@ module.exports = {
     )
 
     const body = { room, equipment }
-    let response = await RoomModel.createRoom(body)
+    let response = await RoomRepo.createRoom(body)
     return response
   },
   update: async data => {
@@ -72,11 +72,11 @@ module.exports = {
 
     const body = { room, equipment }
     console.log('input', body)
-    let response = await RoomModel.updateRoom(body)
+    let response = await RoomRepo.updateRoom(body)
     return response
   },
   delete: async data => {
-    let response = await RoomModel.deleteRoom(data)
+    let response = await RoomRepo.deleteRoom(data)
     return response
   }
 }
