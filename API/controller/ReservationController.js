ec956a19251b3da4b1c404c68d6e8cc0fe43c754
tulathorn <tul.tulathorn@gmail.com>
2019-03-26 16:16:41 +0700

    [EDIT]: Replace checkavaible function

--- API/controller/ReservationController.js
+++ API/controller/ReservationController.js
@@ -6,16 +6,24 @@ module.exports = {
     return await ReservationService.getAllReservations(data)
   },
   create: async data => {
-    let condition = {
-      RoomID: data.RoomID,
-      Date: data.Date,
-      StartTime: data.StartTime,
-      EndTime: data.EndTime
-    }
+    // let condition = {
+    //   RoomID: data.RoomID,
+    //   Date: data.Date,
+    //   StartTime: data.StartTime,
+    //   EndTime: data.EndTime
+    // }
+    // let response = {}
+    // let validReservation = await ReservationService.getAllReservations(condition)
+    // console.log('validReservation in reservation controller', validReservation.length)
     let response = {}
-    let validReservation = await ReservationService.getAllReservations(condition)
-    console.log('validReservation in reservation controller', validReservation.length)
-    if (!validReservation.length === 0) {
+    let checkRoomAvaiable = await ReservationService.checkroomAvaiable(
+      data.RoomID,
+      data.Date,
+      data.StartTime,
+      data.EndTime
+    )
+    console.log(checkRoomAvaiable)
+    if (checkRoomAvaiable === false) {
       response = {
         Code: 1,
         Error: 'Booking exist'
--- API/service/ReservationService.js
+++ API/service/ReservationService.js
@@ -1,6 +1,49 @@
 const { Reservation } = require('../model')
 
 module.exports = {
+  checkroomAvaiable: async (RoomID, date, StartTime, EndTime) => {
+    let check = false
+    const condition = {
+      RoomID,
+      date,
+      StartTime,
+      EndTime
+    }
+    const result = await Reservation.where({
+      RoomID: condition.RoomID,
+      Date: condition.date
+    })
+      .fetchAll()
+      .then(data => {
+        console.log(data.toJSON())
+        return data.toJSON()
+      })
+    if (result.length === 0) {
+      return (check = true)
+    } else {
+      result.map(booking => {
+        let StartTimeMoment = moment(booking.StartTime, 'HH:mm:ss')
+        let EndTimeMoment = moment(booking.EndTime, 'HH:mm:ss')
+        let ConditionStart = moment(condition.StartTime, 'HH:mm:ss')
+        let ConditionEnd = moment(condition.EndTime, 'HH:mm:ss')
+        console.log(
+          (moment(ConditionStart).isBefore(StartTimeMoment) ||
+            moment(ConditionStart).isAfter(EndTimeMoment)) &&
+            (moment(ConditionEnd).isBefore(StartTimeMoment) ||
+              moment(ConditionEnd).isAfter(EndTimeMoment))
+        )
+        if (
+          (moment(ConditionStart).isBefore(StartTimeMoment) ||
+            moment(ConditionStart).isAfter(EndTimeMoment)) &&
+          (moment(ConditionEnd).isBefore(StartTimeMoment) ||
+            moment(ConditionEnd).isAfter(EndTimeMoment))
+        ) {
+          return (check = true)
+        }
+      })
+    }
+    return check
+  },
   getAllReservations: args => {
     return new Promise(async (resolve, reject) => {
       try {
